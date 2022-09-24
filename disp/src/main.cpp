de6bccf27a4c35a66fcd50f4c89706104e39dd5a
Kevin Denker <kevindenker78@gmail.com>
2022-09-24 13:46:24 -0300

    adicionando biblioteca wifi

--- disp/src/main.cpp
+++ disp/src/main.cpp
@@ -1,24 +1,58 @@
 #include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 #include "DHTesp.h"
 
+#define DELAY 7000
+
+//Pinagem
 const int DHT_PIN = 2;
 
+//Posições do display lcd (X & Y)
 int lcdColumns = 20;
 int lcdRows = 4;
 
+//Sensores
 DHTesp temp;
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+
+//Conexão
+String serverName = "https://gees-f59cb-default-rtdb.firebaseio.com/dados.json";
+unsigned long lastUpdate = 0;
 
 void setup(){
   
   Serial.begin(115200);
+
   temp.setup(DHT_PIN, DHTesp::DHT22);
- 
+  
+  // Id da rede e Senha para conexão
+  char* ssid = "";
+  char* password = "";
+
+  WiFi.begin(ssid, password);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(1000);
+  }
+  ssid = "null";
+  password = "null";
+  Serial.print("Conectado com Sucesso");
+
   lcd.init();                     
   lcd.backlight();
 }
 
 void loop(){
+
+  long unsigned int now = millis();
+  if((now - lastUpdate) > DELAY){
+    if(WiFi.status() == WL_CONNECTED){
+      HTTPClient http;
+      http.begin(serverName.c_str());
+      http.addHeader("Content-Type", "application/json");
+    }
+  }
 
   // Sensor de Temperatura
   TempAndHumidity  data = temp.getTempAndHumidity();
@@ -29,7 +63,7 @@ void loop(){
   // Tela LCD
   lcd.setCursor(0, 0);
   lcd.print("Temperatura: " + String(data.temperature, 2));
-  lcd.setCursor(1, 2);
+  lcd.setCursor(0, 2);
   lcd.println("Umidade: " + String(data.humidity, 1));
 
 
